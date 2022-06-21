805b49cea9380083ebe50172f7354398d31080ca
amisha2515 <amishanaik.te19@bmsce.ac.in>
2022-06-21 12:14:31 +0530

    first push

--- voice_rec.py
+++ voice_rec.py
@@ -4,12 +4,15 @@ import pywhatkit
 import datetime
 import wikipedia
 import pyjokes
+import webbrowser
+import os
+
 
 listener=sr.Recognizer()
 engine=pyttsx3.init()
 voices=engine.getProperty('voices')
 engine.setProperty('voice',voices[1].id)
-#engine.say('I am ajay bot. How can I help You')
+engine.say('Hey i am alexa. How can I help You')
 engine.runAndWait()
 
 def talk(text):
@@ -26,15 +29,18 @@ def take_command():
             print(command)
     except:
         print("microphone not working")
+    
     return command
 
 def run_alexa():
     command=take_command()
     print(command)
     if 'play' in command:
-        song=command.replace('play','...')
-        talk('pllaying'+song)
-        pywhatkit.playonyt(song)
+        music = "C:\\Users\\amish\\Music"
+        song=os.listdir(music)
+        print(song)
+        talk('playing music')
+        os.startfile(os.path.join(music,song[0]))
     elif 'time' in command:
         time=datetime.datetime.now().strftime('%I:%M %p')
         talk('time is '+time)
@@ -48,11 +54,26 @@ def run_alexa():
     elif 'joke' in command:
         talk(pyjokes.get_joke())
         print(pyjokes.get_joke())
+    elif ' open youtube ' in command:
+            webbrowser.open('youtube.com')
+        
+    elif 'open google' in command:
+            webbrowser.open('google.com')
+    elif 'play music' in command:
+            music_dir='C:\\Users\\ajayp\\Music'
+            songs=os.listdir(music_dir)
+            print(songs)
+            os.startfile(os.path.join(music_dir, songs[0]))
+    elif 'open vs code' in command:
+            code_dir="C:\\Users\\amish\\OneDrive\\Desktop\\Visual Studio Code.lnk"
+            os.startfile(code_dir)
+    
     elif 'bye'in command:
-        quit
+        talk("okay bye bye. see you soon")
+        quit()
+       
     else:
         talk("please say the command again")
-
         
 while True:
     run_alexa()
